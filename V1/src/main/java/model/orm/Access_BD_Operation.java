d87da20832f1e2178404de75eec7cc7da83a8c9a
Prescilla Estrade <prescilla.estrade@etu.univ-tlse2.fr>
2023-05-24 09:11:17 +0200

    Debit exceptionnel

--- V1/src/main/java/model/orm/Access_BD_Operation.java
+++ V1/src/main/java/model/orm/Access_BD_Operation.java
@@ -188,7 +188,7 @@ public class Access_BD_Operation {
 			Connection con = LogToDatabase.getConnexion();
 			CallableStatement call;
 
-			String q = "{call Debiter (?, ?, ?, ?)}";
+			String q = "{call Debit_Exceptionnel (?, ?, ?, ?)}";
 			// les ? correspondent aux paramètres : cf. déf procédure (4 paramètres)
 			call = con.prepareCall(q);
 			// Paramètres in
