c26ba33bc082d9a6ac209898a8477bc470725397
Julien BERNARD <127470227+julienbernard85@users.noreply.github.com>
2023-05-28 15:17:26 +0200

    Javadoc CategorieOperation.java

--- V1/src/main/java/application/tools/CategorieOperation.java
+++ V1/src/main/java/application/tools/CategorieOperation.java
@@ -4,8 +4,8 @@ package application.tools;
  * Catégories possibles de opérations dans l'application.
  *
  * Simplifie et limite les accès à la BD. Implicite en BD : un type opération
- * est un cas particulier de DEBIT ou CREDIT.
- *
+ * est un cas particulier de DEBIT, DEBITEXCEPTIONNEL, CREDIT ou VIREMENT.
+ *@author Julien Bernard, Prescilla Estrade
  */
 public enum CategorieOperation {
 	DEBIT, DEBITEXCEPTIONNEL, CREDIT, VIREMENT
