72156e4fc42ed2d49792b805c24b0c89f4ffb9e0
Prescilla Estrade <126151905+prescillaestrade@users.noreply.github.com>
2023-05-28 19:16:52 +0200

    emprunt

--- V1/src/main/java/application/view/SimulationEmpruntController.java
+++ V1/src/main/java/application/view/SimulationEmpruntController.java
@@ -23,15 +23,18 @@ public class SimulationEmpruntController {
 		
 		double capitalDeb=capitalEmp;
 		double capitalFin=capitalEmp;
-		double montantAss=(tauxEmp*capitalEmp)/12;
+		double montantAss = (tauxEmp*capitalEmp)/12;
+		if (tauxAss == 0) {
+			montantAss = 0;
+		}
 		double mensualite=(capitalEmp*tauxEmp*Math.pow(1+tauxEmp, dureeEmp)/(Math.pow(1+tauxEmp, dureeEmp)-1));
 		
-		String[] text = {"Mois\n\n\n", "Capital restant du\nen début de mois\n\n", "Montant des intérêts\n\n\n", "Montant du principal\n\n\n",
+		String[] text = {"Numéro période\n\n\n", "Capital restant du\nen début de période\n\n", "Montant des intérêts\n\n\n", "Montant du principal\n\n\n",
 		"Montant à rembourser \nmensualité sans assurance\n\n", "Coût assurance\n\n\n", "Montant à rembourser \nmensualité avec assurance\n\n",
-		"Capital restant du\nen fin de mois\n\n"
+		"Capital restant du\nen fin de période\n\n"
 		};
 		
-		for (int mois=1; mois<dureeEmp; mois++) {
+		for (int mois=0; mois<dureeEmp; mois++) {
 
 			if (capitalDeb!=capitalFin)
 				capitalDeb=capitalFin;
